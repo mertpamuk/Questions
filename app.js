19f18dc266cfbde24a6363f70b64e8d6bf057c7b
mertpamuk <dev.mertpamuk@gmail.com>
2022-02-21 03:30:44 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+let questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    let btn = question.querySelector('.question-btn');
+  
+  btn.addEventListener('click', function(){
+
+    questions.forEach(function(item){
+       if(item !== question){
+           item.classList.remove("show-text")
+       }
+    })
+
+
+      question.classList.toggle('show-text')
+  })
+});
